1c206e937e14acaff54554be869ed7b5f897f022
Dmitrij Fedorenko <c0va23@gmail.com>
2014-06-22 20:36:03 +0400

    Fixed validatoin for tags.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  validates :value, presence: true, uniqueness: { case_sensivite: false }
+  validates :value, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :tag_of_things, dependent: :destroy
   has_many :things, through: :tag_of_things
